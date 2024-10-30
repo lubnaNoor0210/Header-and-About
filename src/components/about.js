043e2348ff5aeaef13bfa7503f820ccc995edfc2
lubnaNoor0210 <lubnanoorulhaq@gmail.com>
2024-10-30 17:25:50 +0500

    initial commit

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>This is the about page of my Next.js app.</p>
+        </div>
+    );
+};
+
+export default About;
